57ac8e231a984e521f368fd60717797871b034e3
bekaBirhanu <152414756+bekaBirhanu@users.noreply.github.com>
2023-12-05 23:59:38 +0300

    Added solution for Toeplitz Matrix

--- week1/toeplitz-matrix.py
+++ week1/toeplitz-matrix.py
@@ -1,7 +1,17 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        for i in range(1, len(matrix)):
+        diagonal_starts = {}
+
+        for i,val in enumerate(matrix[0]):
+            diagonal_starts[i] = val
+
+        for i in range(1,len(matrix)):
+           diagonal_starts[-i] = matrix[i][0]
+            
+        
+        for i in range(len(matrix)):
             for j in range(1, len(matrix[0])):
-                if matrix[i][j] != matrix[i-1][j-1]:
-                    return False
+                    if matrix[i][j] != diagonal_starts[j-i]:
+                        return False
         return True
+       
