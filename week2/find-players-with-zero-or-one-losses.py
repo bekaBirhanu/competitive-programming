6731828311f7f729121cac44751b878dd3bf4dbd
bekaBirhanu <152414756+bekaBirhanu@users.noreply.github.com>
2023-12-11 17:37:01 +0300

    Added solution for Find Players With Zero or One Losses

--- week2/find-players-with-zero-or-one-losses.py
+++ week2/find-players-with-zero-or-one-losses.py
@@ -1,13 +1,18 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        winners = set()
-        losers_one_times = set()
-        losers_more_times = set()
+        
+        winners = set() # store players with any win
+        losers_one_times = set() # stores players exactly with any lose
+        losers_more_times = set() # stores players with more than one loses
+
         for winner, loser in matches:
             winners.add(winner)
             if loser in losers_one_times:
                 losers_more_times.add(loser)
             losers_one_times.add(loser)
             
-
+        """
+        no lose = any win - any lose
+        one lose = any lose - many lose
+        """
         return [sorted(list(winners- losers_one_times)),sorted(list(losers_one_times-losers_more_times))]
