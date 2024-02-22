e3a0d9a99ebf59fecdf576c374733eb1d1138924
bekaBirhanu <152414756+bekaBirhanu@users.noreply.github.com>
2024-02-22 07:05:50 +0300

    Add solution for ReverseString

---
+++ camp1/week1/LeetCode/ReverseString.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def reverse_margines(s,i,j):
+            if i >= j:
+                return
+            s[i], s[j] = s[j], s[i]
+            reverse_margines(s,i+1,j-1)
+
+        reverse_margines(s,0,len(s)-1)
+        """
+        Do not return anything, modify s in-place instead.
+        """
